7960431ed4e01299e1c186975a1304080121c353
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-03 16:15:59 +0900

    eslint 공부

--- eslint-test/.eslintrc.js
+++ eslint-test/.eslintrc.js
@@ -1,12 +1,13 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    semi: ["error", "always"],
+  },
 };
