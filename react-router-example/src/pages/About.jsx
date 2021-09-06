5ffb61d4b806f87d46cff9771a09589ff65ce145
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-06 17:52:04 +0900

    queryString 추가

--- react-router-example/src/pages/About.jsx
+++ react-router-example/src/pages/About.jsx
@@ -1,3 +1,11 @@
-export default function About() {
-  return <div>About 페이지입니다.</div>;
+export default function About(props) {
+  const searchParams = props.location.search;
+  console.log(searchParams);
+  const obj = new URLSearchParams(searchParams);
+  console.log(obj.get("name"));
+  return (
+    <div>
+      <h2>About 페이지입니다.</h2>
+    </div>
+  );
 }
