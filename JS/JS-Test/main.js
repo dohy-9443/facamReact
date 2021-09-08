aa4275394fee76c00cfccf0e5c8fb3232631a8e1
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-08 16:08:51 +0900

    산술 연산자

--- JS/JS-Test/main.js
+++ JS/JS-Test/main.js
@@ -1,17 +1,7 @@
-console.log(typeof "hello world");
-console.log(typeof 123);
-console.log(typeof true);
-console.log(typeof undefined);
-console.log(typeof null);
-console.log(typeof {});
-console.log(typeof []);
+// 산술 연산자(arithmetic operator)
 
-function getType(data) {
-  return Object.prototype.toString.call(data).slice(8, -1);
-}
-
-console.log(getType(123));
-console.log(getType(false));
-console.log(getType(null));
-console.log(getType({}));
-console.log(getType([]));
+console.log(1 + 2);
+console.log(5 - 7);
+console.log(3 * 4);
+console.log(10 / 2);
+console.log(7 % 5);
