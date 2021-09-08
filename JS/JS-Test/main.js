1d87c839ef8e75089e4b52c60c8d09e89ee74bf7
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-08 15:58:42 +0900

    어떤 타입인지 제대로 알아보기 위해 함수를 만듬

--- JS/JS-Test/main.js
+++ JS/JS-Test/main.js
@@ -5,3 +5,13 @@ console.log(typeof undefined);
 console.log(typeof null);
 console.log(typeof {});
 console.log(typeof []);
+
+function getType(data) {
+  return Object.prototype.toString.call(data).slice(8, -1);
+}
+
+console.log(getType(123));
+console.log(getType(false));
+console.log(getType(null));
+console.log(getType({}));
+console.log(getType([]));
