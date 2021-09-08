ef62c9eda64935afa92e2ac2423fd2b2df7d14b6
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-08 16:11:35 +0900

    할당 연산자

--- JS/JS-Test/main.js
+++ JS/JS-Test/main.js
@@ -1,7 +1,24 @@
-// 산술 연산자(arithmetic operator)
+// 할당 연산자(assignment operator)
 
-console.log(1 + 2);
-console.log(5 - 7);
-console.log(3 * 4);
-console.log(10 / 2);
-console.log(7 % 5);
+let a = 2;
+// a = a + 1;
+
+a += 1;
+
+console.log(a);
+
+a -= 1;
+
+console.log(a);
+
+a *= 2;
+
+console.log(a);
+
+a /= 2;
+
+console.log(a);
+
+a %= 2;
+
+console.log(a);
