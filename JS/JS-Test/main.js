5d262aec4391f81852622aa53bf03ff13bcd6eab
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-09 15:59:29 +0900

    호이스팅

--- JS/JS-Test/main.js
+++ JS/JS-Test/main.js
@@ -1,13 +1,18 @@
-// 즉시실행함수
-// IIFE, Immediately-Invoked Function Expression
+// 호이스팅(Hoisting)
+// 함수 선언부가 유효범위 최상단으로 끌어올려지는 현상
+
+const Hello = document.querySelector("h1");
 
 const a = 7;
+
+Hello.addEventListener("click", () => {
+  play();
+});
+
+function play() {
+  double();
+}
+
 function double() {
   console.log(a * 2);
 }
-
-double();
-
-(function () {
-  console.log(a * 2);
-})();
