5c28b88a22573668b806df04d2e53de4729ca87f
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-09 14:40:19 +0900

    변수 유효범위

--- JS/JS-Test/main.js
+++ JS/JS-Test/main.js
@@ -1,15 +1,11 @@
-// 반복문 (For statement)
-// for (시작조건; 종료조건; 변화조건;) {}
+// 변수 유효범위(Variable Scope)
+// var , let , const
 
-const ulEl = document.querySelector("ul");
-
-for (let i = 1; i <= 10; i++) {
-  const li = document.createElement("li");
-  li.textContent = `list-${i}`;
-  if (i % 2 === 0) {
-    li.addEventListener("click", () => {
-      console.log(li.textContent);
-    });
+function scope() {
+  if (true) {
+    const a = 123;
   }
-  ulEl.appendChild(li);
+  console.log(a);
 }
+
+scope();
