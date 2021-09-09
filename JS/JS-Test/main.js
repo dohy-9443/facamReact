e60ead750362d06a5282dfd1755466a9f4b56118
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-09 14:53:48 +0900

    형 변환

--- JS/JS-Test/main.js
+++ JS/JS-Test/main.js
@@ -1,11 +1,17 @@
-// 변수 유효범위(Variable Scope)
-// var , let , const
+// 형 변환 (Type conversion)
 
-function scope() {
-  if (true) {
-    const a = 123;
-  }
-  console.log(a);
+const a = 1;
+const b = "1";
+
+console.log(a === b); // 일치 연산자
+console.log(a == b); // 동등 연산자 :: 왠만하면 쓰지마 ::
+
+// Truthy (참 같은 값)
+// True, {}, [], 1, 2, 'false', -12, '3.14' ...
+
+// Falsy (거짓 같은 값)
+// false, '', null, undefined, 0, -0, NaN(Not a Number)
+
+if (0) {
+  console.log(123);
 }
-
-scope();
