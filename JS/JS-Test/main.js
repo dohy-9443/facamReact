64ccc1b4f3cee57d125ebbb8fbb8e5691f95ebf6
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-10 14:27:41 +0900

    timer에서 this (function)

--- JS/JS-Test/main.js
+++ JS/JS-Test/main.js
@@ -2,26 +2,13 @@
 // 일반(Normal) 함수는 호출 위치에서 따라 this 정의!
 // 화살표(Arrow) 함수는 자신이 선언된 함수 범위에서 this 정의!
 
-function User(name) {
-  this.name = name;
-}
-User.prototype.normal = function () {
-  console.log(this.name);
-};
-User.prototype.arrow = () => {
-  console.log(this.name);
-};
-
-const donghyun = new User("donghyun");
-
-donghyun.normal();
-donghyun.arrow();
-
-const amy = {
-  name: "Amy",
-  normal: donghyun.normal,
-  arrow: donghyun.arrow,
+const timer = {
+  name: "donghyun",
+  timeout: function () {
+    setTimeout(function () {
+      console.log(this.name);
+    }, 1000);
+  },
 };
 
-amy.normal();
-amy.arrow();
+timer.timeout();
