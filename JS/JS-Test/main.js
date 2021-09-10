7f8e27a21fd241f6c9b63f90ff23951e4df528a4
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-10 18:27:21 +0900

    배열 1

--- JS/JS-Test/main.js
+++ JS/JS-Test/main.js
@@ -8,35 +8,55 @@
 // Array: []
 // Object: {}
 
-const pi = 3.14159265358979;
-console.log(pi);
+const numbers = [1, 2, 3, 4];
+const fruits = ["Apple", "Banana", "Cherry"];
 
-const str = pi.toFixed(2);
-// toFixed(2)는 소수점아래 2자리까지
-// (이거 쓰면 문자데이터로 바뀜)
-console.log(str);
-console.log(typeof str);
+console.log(numbers[1]);
+console.log(fruits);
 
-const integer = parseInt(str);
-// parseInt는 정수형으로 바꿔줌
-const float = parseFloat(str);
-// parseFloat는 실수형으로 바꿔줌
-console.log(integer);
-console.log(float);
-console.log(typeof integer, typeof float);
+// .length
+console.log(numbers.length);
+console.log(fruits.length);
+console.log([1, 2].length);
+console.log([].length);
 
-// Math
-console.log("abs: ", Math.abs(-12));
-// abs 절대값
-console.log("min: ", Math.min(2, 8));
-// min 최소값
-console.log("max: ", Math.max(2, 8));
-// max 최대값
-console.log("ceil", Math.ceil(3.14));
-// ceil 올림
-console.log("floor: ", Math.floor(3.14));
-// floor 내림
-console.log("round: ", Math.round(3.14));
-// round 반올림
-console.log("random: ", Math.random());
-// 0 ~ 1 사이의 랜덤값
+// .concat()
+// 두개의 배열 데이터를 병합하여 새로운 배열 데이터를
+// 그 자리에 반환 해줌
+// 원본 데이터는 영향이 없음
+console.log(numbers.concat(fruits));
+console.log(numbers);
+console.log(fruits);
+
+// .forEach()
+fruits.forEach(function (element, index, array) {
+  console.log(element, index, array);
+});
+// 지금 보면 fruits에 사과 바나나 체리 있다.
+// 배열의 요소만큼 반복해서 도는데 첫번째 element에
+// 사과가 들어가면서 그 인덱스 번호는 0번 array는
+// 현재 참조하고있는 배열을 내보낸다. 그래서 결과가
+// 아래처럼 나온다.
+
+// Apple 0 ['Apple', 'Banana', 'Cherry']
+// Banana 1 ['Apple', 'Banana', 'Cherry']
+// Cherry 2 ['Apple', 'Banana', 'Cherry']
+
+// .map()
+// map은 forEach와 다르게 return으로 내보낸다.
+// 반복해서 돌리고 나온 데이터들을 반환해
+// 새로운 배열로서 b에 담는다.
+
+const a = fruits.forEach((fruit, index) => {
+  console.log(`${fruit}-${index}`);
+});
+console.log(a);
+
+const b = fruits.map((fruit, index) => {
+  return {
+    id: index,
+    name: fruit,
+  };
+});
+
+console.log(b);
