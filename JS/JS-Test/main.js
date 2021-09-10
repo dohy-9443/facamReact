ed28ab88d9096a152435362a63267b3431320fbc
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-10 14:24:18 +0900

    클래스에서 this

--- JS/JS-Test/main.js
+++ JS/JS-Test/main.js
@@ -2,15 +2,18 @@
 // 일반(Normal) 함수는 호출 위치에서 따라 this 정의!
 // 화살표(Arrow) 함수는 자신이 선언된 함수 범위에서 this 정의!
 
-const donghyun = {
-  name: "donghyun",
-  normal: function () {
-    console.log(this.name);
-  },
-  arrow: () => {
-    console.log(this.name);
-  },
+function User(name) {
+  this.name = name;
+}
+User.prototype.normal = function () {
+  console.log(this.name);
 };
+User.prototype.arrow = () => {
+  console.log(this.name);
+};
+
+const donghyun = new User("donghyun");
+
 donghyun.normal();
 donghyun.arrow();
 
