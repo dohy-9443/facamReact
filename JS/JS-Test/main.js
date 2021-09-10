711c8eb4b57b1f9729ff2aeaea3cfd3a8bc86f21
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-10 14:54:42 +0900

    ES6 class 써보기

--- JS/JS-Test/main.js
+++ JS/JS-Test/main.js
@@ -1,14 +1,17 @@
-// this
-// 일반(Normal) 함수는 호출 위치에서 따라 this 정의!
-// 화살표(Arrow) 함수는 자신이 선언된 함수 범위에서 this 정의!
+// ES6 Classes
 
-const timer = {
-  name: "donghyun",
-  timeout: function () {
-    setTimeout(() => {
-      console.log(this.name);
-    }, 1000);
-  },
-};
+class User {
+  constructor(first, last) {
+    this.first = first;
+    this.last = last;
+  }
+  getFullName = () => {
+    return `${this.first} ${this.last}`;
+  };
+}
 
-timer.timeout();
+const dong = new User("DongHyun", "Baek");
+const amy = new User("Amy", "Clarke");
+
+console.log(dong.getFullName());
+console.log(amy.getFullName());
