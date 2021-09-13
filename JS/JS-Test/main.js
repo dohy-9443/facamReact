6282d0b7961155b6a27e2fea5d81c196757a1fca
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-13 15:52:01 +0900

    불변성2

--- JS/JS-Test/main.js
+++ JS/JS-Test/main.js
@@ -1,9 +1,12 @@
-let a = 1;
-let b = 4;
+let a = { k: 1 };
+let b = { K: 1 };
 console.log(a, b, a === b);
+a.k = 7;
 b = a;
 console.log(a, b, a === b);
-a = 7;
+a.k = 2;
 console.log(a, b, a === b);
-let c = 1;
-console.log(b, c, b === c);
+let c = b;
+console.log(a, b, c, a === c);
+a.k = 9;
+console.log(a, b, c, a === c);
