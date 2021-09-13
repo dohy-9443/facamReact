9c3c844edbff8b1b47ba10d19ab011dc6c03b9a8
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-13 17:50:56 +0900

    얕은 복사와 깊은 복사 1

--- JS/JS-Test/main.js
+++ JS/JS-Test/main.js
@@ -1,12 +1,16 @@
-let a = { k: 1 };
-let b = { K: 1 };
-console.log(a, b, a === b);
-a.k = 7;
-b = a;
-console.log(a, b, a === b);
-a.k = 2;
-console.log(a, b, a === b);
-let c = b;
-console.log(a, b, c, a === c);
-a.k = 9;
-console.log(a, b, c, a === c);
+// 얕은 복사(Shallow copy), 깊은 복사(Deep copy)
+const user = {
+  name: "donghyun",
+  age: 27,
+  emails: ["dong944320@gamil.com"],
+};
+const copyUser = user;
+console.log(copyUser === user);
+
+user.age = 26;
+console.log("user", user);
+console.log("copyUser", copyUser);
+
+console.log("---------------");
+console.log("---------------");
+console.log(copyUser === user);
