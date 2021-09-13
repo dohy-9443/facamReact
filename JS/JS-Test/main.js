369fdf4ccfcb35731b97449965d0970cb0477756
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-13 14:22:03 +0900

    객체2

--- JS/JS-Test/main.js
+++ JS/JS-Test/main.js
@@ -18,6 +18,9 @@ const userEmail = {
   email: "dong944320@gmail,com",
 };
 
+// .assign()
+// 하나 이상의 원본 객체들로부터
+// 모든 열거가능한 속성들을 대상 객체로 복사
 const target = Object.assign(userAge, userEmail);
 console.log(target);
 console.log(userAge);
@@ -26,3 +29,18 @@ console.log(target === userAge);
 const a = { k: 123 };
 const b = { k: 123 };
 console.log(a === b);
+
+const user = {
+  name: "donghyun",
+  age: 27,
+  email: "dong944320@gmail.com",
+};
+
+const keys = Object.keys(user);
+console.log(keys);
+// ['name', 'age', 'email']
+
+console.log(user["email"]);
+
+const values = keys.map((key) => user[key]);
+console.log(values);
