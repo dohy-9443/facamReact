c830d1d2b03d0c9400abfbee8acf24433792648b
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-13 15:44:15 +0900

    데이터 불변성 1

--- JS/JS-Test/main.js
+++ JS/JS-Test/main.js
@@ -1,18 +1,9 @@
-// 전개 연산자 (Spread)
-
-const fruits = ["Apple", "Banana", "Cherry", "Dragon fruit"];
-console.log(fruits);
-console.log(...fruits);
-// console.log('Apple', 'Banana', 'Cherry')
-
-// function toObject(a, b, ...c) { // ...c는 rest parameter
-//   return {
-//     apple: a,
-//     banana: b,
-//     rest: c,
-//   };
-// }
-
-const toObject = (a, b, ...c) => ({ a, b, c });
-
-console.log(toObject(...fruits));
+let a = 1;
+let b = 4;
+console.log(a, b, a === b);
+b = a;
+console.log(a, b, a === b);
+a = 7;
+console.log(a, b, a === b);
+let c = 1;
+console.log(b, c, b === c);
