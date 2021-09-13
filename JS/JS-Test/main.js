7e59a125996232f97d84f94c55724efacc318e86
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-13 18:00:08 +0900

    깊은 복사 lodash

--- JS/JS-Test/main.js
+++ JS/JS-Test/main.js
@@ -1,10 +1,12 @@
+import _ from "lodash";
+
 // 얕은 복사(Shallow copy), 깊은 복사(Deep copy)
 const user = {
   name: "donghyun",
   age: 27,
   emails: ["dong944320@gamil.com"],
 };
-const copyUser = { ...user };
+const copyUser = _.cloneDeep(user);
 console.log(copyUser === user);
 
 user.age = 26;
