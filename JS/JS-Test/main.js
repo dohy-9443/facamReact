b918a3791ec1f9f9496729f6c180611877781adc
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-14 15:29:40 +0900

    storage

--- JS/JS-Test/main.js
+++ JS/JS-Test/main.js
@@ -1,23 +1,14 @@
-// JSON (JavaScript Object Notation)
-// 자바스크립트의 객체 표기법
-
-import myData from "./myData.json";
-
-console.log(myData);
-
 const user = {
-  name: "DongHyun",
+  name: "DONGHYUN",
   age: 27,
   emails: ["dong944320@gmail.com", "neo@zillinks.com"],
 };
-console.log("user", user);
 
-const str = JSON.stringify(user);
-// javaScript 값이나 객체를 JSON 문자열로 변환
-console.log("str", str);
-console.log(typeof str);
+localStorage.setItem("user", JSON.stringify(user));
+// setItem('데이터 이름', 저장할 객체 데이터)
 
-const obj = JSON.parse(str);
-// JSON 문자열의 구문을 분석하고,
-// 그 결과에서 JavaScript 값이나 객체를 생성
-console.log("obj", obj);
+console.log(JSON.parse(localStorage.getItem("user")));
+// getItem은 localStorage에 있는 데이터를 가져와서 보는거
+
+localStorage.removeItem("user");
+// removeItem()은 데이터 지우기
