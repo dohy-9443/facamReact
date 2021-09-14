f5dee7f32377793efa0b26903232d48a3adbb1c8
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-14 15:14:03 +0900

    json

--- JS/JS-Test/main.js
+++ JS/JS-Test/main.js
@@ -1,37 +1,20 @@
-import _ from "lodash";
+// JSON (JavaScript Object Notation)
+// 자바스크립트의 객체 표기법
 
-const usersA = [
-  { userId: "1", name: "Donghyun" },
-  { userId: "2", name: "Neo" },
-];
-const usersB = [
-  { userId: "1", name: "Donghyun" },
-  { userId: "3", name: "Amy" },
-];
-const usersC = usersA.concat(usersB);
-// 인자로 주어진 배열이나 값들을 기존 배열에 합쳐서 새 배열을 반환
-console.log("concat", usersC);
-console.log("uniqBy", _.uniqBy(usersC, "userId"));
-// 위아래 둘다 마지막 인자는 고유값으로 쓸거
-// 위에꺼는 배열데이터가 하나일 때 쓰는거
-// 아래꺼는 여러개일 때 쓰는 거
-const usersD = _.unionBy(usersA, usersB, "userId");
-console.log("unionBy", usersD);
+import myData from "./myData.json";
 
-console.log("--------------------");
+console.log(myData);
 
-const users = [
-  { userId: "1", name: "Donghyun" },
-  { userId: "2", name: "Neo" },
-  { userId: "3", name: "Amy" },
-  { userId: "4", name: "Evan" },
-  { userId: "5", name: "Lewis" },
-];
+const user = {
+  name: "DongHyun",
+  age: 27,
+  emails: ["dong944320@gmail.com", "neo@zillinks.com"],
+};
+console.log("user", user);
 
-const foundUser = _.find(users, { name: "Amy" });
-const foundUserIndex = _.findIndex(users, { name: "Amy" });
-console.log(foundUser);
-console.log(foundUserIndex);
+const str = JSON.stringify(user);
+console.log("str", str);
+console.log(typeof str);
 
-_.remove(users, { name: "Donghyun" });
-console.log(users);
+const obj = JSON.parse(str);
+console.log("obj", obj);
