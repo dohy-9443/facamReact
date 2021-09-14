698009c1bee691934a9dc3d050c95282d3005698
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-14 19:25:55 +0900

    복구

--- JS/JS-Test/main.js
+++ JS/JS-Test/main.js
@@ -1,1 +1,16 @@
-// 내용이 변경됨
+import axios from "axios";
+
+function fetchMobies() {
+  axios.get("https://www.omdbapi.com/?apikey=7035c60c&s=frozen").then((res) => {
+    console.log(res);
+    const h1El = document.querySelector("h1");
+    const imgEl = document.querySelector("img");
+    h1El.textContent = res.data.Search[0].Title;
+    imgEl.src = res.data.Search[0].Poster;
+  });
+}
+
+fetchMobies();
+
+// 커밋해보기
+// 변경사항
