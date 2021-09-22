af45667b3aa76f1f3c3fffd2f3fe8a672ead6569
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-22 23:51:30 +0900

    실무에 바로 적용하는 자바스크립트 코드 레시피 278공부

---
+++ JS/CodeRecipe278/chapter1/006.js
@@ -0,0 +1,34 @@
+// 변수 myName에 '동현'을 대입
+let myName = "동현";
+// myName의 값을 console로 출력
+console.log(myName);
+
+let myString = "동현"; // 문자열
+let currentDate = new Date(); // 현재 날짜
+let myFunction = () => console.log("함수 확인"); // 함수
+
+// 대입값 변경
+let name = "펭귄";
+name = "고래";
+console.log(name);
+
+// 숫자 타입 변수 간에는 덧셈과 뺄셈 계산 가능
+// 문자열 타입 변수 간에는 결합이 가능
+// 한 변수는 다른 변수에 대입 가능
+
+let num1 = 10;
+let num2 = 20;
+let sum = num1 + num2;
+console.log(sum);
+
+// 문자열 변수의 결합
+let firstName = "브렌던";
+let familyName = "아이크";
+let fullName = firstName + familyName;
+console.log(fullName);
+
+// 변수를 다른 변수에 대입
+let value1 = 100;
+let value2 = value1;
+
+console.log(value2);
