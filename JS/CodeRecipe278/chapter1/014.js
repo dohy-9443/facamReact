65b514dec0dbc02a37b0beae113f16b85b354170
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-23 21:46:24 +0900

    실무에 바로 적용하는 자바스크립트 코드 레시피 278 1챕터 끝

---
+++ JS/CodeRecipe278/chapter1/014.js
@@ -0,0 +1,23 @@
+// 다수의 파라미터를 가지는 함수 정의하기
+
+/**
+ * 파라미터의 합계를 반환하는 함수
+ * @param prices
+ * @returns {number}
+ */
+
+function calcSum(...prices) {
+  let result = 0;
+
+  for (const value of prices) {
+    result += value;
+  }
+
+  return result;
+}
+
+const result1 = calcSum(10, 20);
+console.log(result1);
+
+const result2 = calcSum(5, 10, 15);
+console.log(result2);
---
+++ JS/CodeRecipe278/chapter1/018.js
@@ -0,0 +1,9 @@
+// 반복문 사용하기 while
+
+let num = 0;
+
+while (num < 10) {
+  console.log(num);
+
+  num++;
+}
