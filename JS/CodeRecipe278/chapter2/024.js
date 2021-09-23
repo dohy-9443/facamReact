3360b22d609ab8d8472153f3adaf8b0acbffed1f
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-24 00:43:50 +0900

    025 완료

---
+++ JS/CodeRecipe278/chapter2/024.js
@@ -0,0 +1,8 @@
+// 수학 계산 함수 사용하기
+
+console.log(Math.abs(-10)); // -10의 절댓값은 10
+console.log(Math.pow(2, 10)); // 2의 10승은 1024
+console.log(Math.sign(2)); // 2가 양수이므로 1
+console.log(Math.sign(-2)); // -2가 음수이므로 -1
+console.log(Math.sqrt(16)); // 16의 제곱근은 4
+console.log(Math.E); // 자연 로그 e는 1
