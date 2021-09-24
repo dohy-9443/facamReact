c25f473580d5881733cff3977216bab3445a616b
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-24 16:07:34 +0900

    오타 확인

--- JS/CodeRecipe278/chapter2/031/031.js
+++ JS/CodeRecipe278/chapter2/031/031.js
@@ -3,10 +3,10 @@
 console.log("JavaScript".charAt(3));
 console.log("JavaScript1".charAt()); // 인수 생략 시 기본값은 0의 위치
 
-// 검색어
+/** 검색어 */
 const searchWordText = document.querySelector("#search-word-input");
 
-// 지역 리스트
+/** 지역 리스트 */
 const prefectureList = document.querySelectorAll("#prefecture-list button");
 
 // 문자가 입력될 때마다 데이터 체크 작업 실행
@@ -15,10 +15,9 @@ searchWordText.addEventListener("keyup", () => {
   const searchWord = searchWordText.value;
 
   // 지역 리스트의 루프 처리
-  // element는 각 요소에 해당됨
+  // element는 각 요소에 해당함
   prefectureList.forEach((element) => {
     // 검색어가 없으면 모든 요소를 표시
-
     if (!searchWord || searchWord === "") {
       element.classList.remove("hide");
       return;
@@ -29,16 +28,16 @@ searchWordText.addEventListener("keyup", () => {
     // 데이터 영문 name 가져오기
     const phonetic = element.dataset.phonetic;
 
-    // 검색어와 첫 번째 글자 일치 여부에 따라 hide 클래스 사용 결정
+    // 검색어와 첫번째 글자 일치 여부에 따라 hide 클래스 사용 결정
     // hide 클래스가 사용된 요소는 화면에 표시하지 않음
     if (
       searchWord.charAt(0) === prefectureName.charAt(0) ||
       searchWord.charAt(0) === phonetic.charAt(0)
     ) {
-      // 검색어의 첫 번째 글자가 일치하는 경우 hide 클래스를 제거
+      // 검색어의 첫번째 글자가 일치하는 경우 hide 클래스를 제거
       element.classList.remove("hide");
     } else {
-      // 검색어의 첫 번째 글자가 일치하지 않는 경우 hide 클래스 추가
+      // 검색어의 첫번째 글자가 일치하지 않는 경우 hide 클래스를 추가
       element.classList.add("hide");
     }
   });
